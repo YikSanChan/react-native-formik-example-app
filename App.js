1beb8b513a67eb37d93515661eb7886c50dc0c03
Evan Chan <evan.chanyiksan@gmail.com>
2019-01-11 02:32:09 -0800

    Add DatePickerInputField

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Picker, StyleSheet, View } from "react-native";
+import {
+  DatePickerIOS,
+  Picker,
+  ScrollView,
+  StyleSheet,
+  View
+} from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import {
@@ -29,6 +35,14 @@ const PickerInputField = ({ label, value, error, handleChange, options }) => (
   </View>
 );
 
+const DatePickerInputField = ({ label, value, error, handleChange }) => (
+  <View>
+    <FormLabel>{label}</FormLabel>
+    <DatePickerIOS date={value} onDateChange={handleChange} />
+    <FormValidationMessage>{error}</FormValidationMessage>
+  </View>
+);
+
 export default class App extends React.Component {
   handleSubmit = values => {
     alert(JSON.stringify(values, null, 2));
@@ -36,13 +50,14 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Formik
           initialValues={{
             name: "iPhone X",
             price: "1000",
             owner: "jackdorsey@twitter.com",
-            status: "0"
+            status: "0",
+            purchasedAt: new Date()
           }}
           isInitialValid={true}
           onSubmit={this.handleSubmit}
@@ -56,36 +71,42 @@ export default class App extends React.Component {
               .required()
           })}
         >
-          {({ handleChange, handleSubmit, values, errors, isValid }) => (
+          {({ setFieldValue, handleSubmit, values, errors, isValid }) => (
             <View>
               <TextInputField
                 label="name"
                 value={values.name}
                 error={errors.name}
-                handleChange={handleChange("name")}
+                handleChange={value => setFieldValue("name", value)}
               />
               <TextInputField
                 label="price"
                 value={values.price}
                 error={errors.price}
-                handleChange={handleChange("price")}
+                handleChange={value => setFieldValue("price", value)}
               />
               <TextInputField
                 label="owner"
                 value={values.owner}
                 error={errors.owner}
-                handleChange={handleChange("owner")}
+                handleChange={value => setFieldValue("owner", value)}
               />
               <PickerInputField
                 label="status"
                 value={values.status}
                 error={errors.status}
-                handleChange={handleChange("status")}
+                handleChange={value => setFieldValue("status", value)}
                 options={[
                   { label: "New", value: "0" },
                   { label: "Donated", value: "1" },
                   { label: "Ebay", value: "2" }
                 ]}
+              />
+              <DatePickerInputField
+                label="purchasedAt"
+                value={values.purchasedAt}
+                error={errors.purchasedAt}
+                handleChange={value => setFieldValue("purchasedAt", value)}
               />
               <Button
                 title="Submit"
@@ -95,7 +116,7 @@ export default class App extends React.Component {
             </View>
           )}
         </Formik>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -103,8 +124,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   }
 });
